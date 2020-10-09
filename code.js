8ae5da3e5de5ed32c6c0c5dd8412a49012950e34
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2020-10-08 19:50:11 -0700

    Emulating private methods with closures

--- code.js
+++ code.js
@@ -459,29 +459,29 @@
 // console.log(reversedString('Emil', 4))
 
 // Emulating private methods with closures
-// let counter = (function() {
-// 	let privateCounter = 0;
-// 	function changeBy(val) {
-// 		privateCounter += val;
-// 	}
+let counter = (function() {
+	let privateCounter = 0;
+	function changeBy(val) {
+		privateCounter += val;
+	}
 
-// 	return {
-// 		increment: function() {
-// 			changeBy(1);
-// 		},
-// 		decrement: function() {
-// 			changeBy(-1);
-// 		},
-// 		value: function() {
-// 			return privateCounter;
-// 		}
-// 	};
-// })();
-// counter.increment();
-// counter.increment();
-// console.log(counter.value());
-// counter.decrement();
-// console.log(counter.value());
+	return {
+		increment: function() {
+			changeBy(1);
+		},
+		decrement: function() {
+			changeBy(-1);
+		},
+		value: function() {
+			return privateCounter;
+		}
+	};
+})();
+counter.increment();
+counter.increment();
+console.log(counter.value());
+counter.decrement();
+console.log(counter.value());
 
 
 
