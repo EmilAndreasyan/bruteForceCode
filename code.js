fd235ab2294c8d384265a1debc8efa13e192a8ca
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2020-10-20 17:34:41 -0700

    new map

--- code.js
+++ code.js
@@ -561,9 +561,20 @@
 // console.log(Array.from('foo'));
 // console.log(Array.from([1, 2, 3], x => x + x));
 
-const set = new Set(['hello', 'goodbye', 'david', 'hello']);
-Array.from(set);
-// [ "hello", "goodbye", "david" ]
+// const set = new Set(['hello', 'goodbye', 'david', 'hello']);
+// Array.from(set);
+// // [ "hello", "goodbye", "david" ]
+
+const map = new Map([[1, 2], [2, 4], [4, 8]]);
+Array.from(map);
+// [[1, 2], [2, 4], [4, 8]]
+
+const mapper = new Map([['1', 'a'], ['2', 'b']]);
+Array.from(mapper.values());
+// ['a', 'b'];
+
+Array.from(mapper.keys());
+// ['1', '2'];
 
 
 
