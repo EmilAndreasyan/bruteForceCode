b39bf03878b9d3a6c3aa2ffdb7ee3d88a85dd60a
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2020-10-21 19:04:36 -0700

    array from arguments

--- code.js
+++ code.js
@@ -565,18 +565,24 @@
 // Array.from(set);
 // // [ "hello", "goodbye", "david" ]
 
-const map = new Map([[1, 2], [2, 4], [4, 8]]);
-Array.from(map);
-// [[1, 2], [2, 4], [4, 8]]
+// const map = new Map([[1, 2], [2, 4], [4, 8]]);
+// Array.from(map);
+// // [[1, 2], [2, 4], [4, 8]]
 
-const mapper = new Map([['1', 'a'], ['2', 'b']]);
-Array.from(mapper.values());
-// ['a', 'b'];
+// const mapper = new Map([['1', 'a'], ['2', 'b']]);
+// Array.from(mapper.values());
+// // ['a', 'b'];
 
-Array.from(mapper.keys());
-// ['1', '2'];
+// Array.from(mapper.keys());
+// // ['1', '2'];
 
-
+function f() {
+    return Array.from(arguments);
+  }
+  
+  f(1, 2, 3);
+  
+  // [ 1, 2, 3 ]
 
 
 
