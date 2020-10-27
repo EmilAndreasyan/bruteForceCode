845d5ed0cbe2b8a82c3a29de492c232ecba167bc
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2020-10-26 18:07:29 -0700

    this

--- code.js
+++ code.js
@@ -594,21 +594,61 @@
 // Array.from({length: 5}, (v, i) => i);
 // // [0, 1, 2, 3, 4]
 
-// Sequence generator function (commonly referred to as "range", e.g. Clojure, PHP etc)
-const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
+// // Sequence generator function (commonly referred to as "range", e.g. Clojure, PHP etc)
+// const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
 
-// Generate numbers range 0..4
-range(0, 4, 1);
-// [0, 1, 2, 3, 4] 
+// // Generate numbers range 0..4
+// range(0, 4, 1);
+// // [0, 1, 2, 3, 4] 
 
-// Generate numbers range 1..10 with step of 2 
-range(1, 10, 2); 
-// [1, 3, 5, 7, 9]
+// // Generate numbers range 1..10 with step of 2 
+// range(1, 10, 2); 
+// // [1, 3, 5, 7, 9]
 
-// Generate the alphabet using Array.from making use of it being ordered as a sequence
-range('A'.charCodeAt(0), 'Z'.charCodeAt(0), 1).map(x => String.fromCharCode(x));
-// ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+// // Generate the alphabet using Array.from making use of it being ordered as a sequence
+// range('A'.charCodeAt(0), 'Z'.charCodeAt(0), 1).map(x => String.fromCharCode(x));
+// // ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
 
+class Country {
+    constructor(name, capital, passenger) {
+        this.name = name.charAt(0).toUpperCase() + name.slice(1);
+        this.capital = capital.charAt(0).toUpperCase() + capital.slice(1);
+        this.passenger = passenger.charAt(0).toUpperCase() + passenger.slice(1)
+        this.sayHello = function() {
+            return `Hello, ${this.passenger}, welcome to the capital of ${this.name} ${this.capital}. Type of "this" is ${typeof this}`
+          }
+    }
+}
+
+let france = new Country("france", "paris", 'john')
+// console.log(france.capital)
+// console.log(france.sayHello())
+const greet = france.sayHello()
+console.log(greet)
+//console.log(france.sayHello.prototype)
+
+// function person(){
+//     console.log(`${this.name} is ${this.age} years old`) // this becomes the variable it is called on
+// }
+
+// let john = {
+//     name: "John",
+//     age: 34
+// }
+
+// person.call(john)
+
+// function person(){
+//     console.log(`${this.name} is ${this.age} years old`) // this becomes the variable it is called on
+// }
+
+// let john = {
+//     name: "John",
+//     age: 34
+// }
+
+// person = person.bind(john)
+// person()
 
 
 
