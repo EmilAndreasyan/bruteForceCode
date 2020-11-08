016140c3748e22e23c24cf4df63cd9168010d519
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2020-11-07 17:24:03 -0800

    array of function

--- code.js
+++ code.js
@@ -731,8 +731,20 @@
 // Array.isArray('foobar');   // false
 // Array.isArray(undefined);  // false
 
-Array.of(7);       // [7] 
-Array.of(1, 2, 3); // [1, 2, 3]
+// Array.of(7);       // [7] 
+// Array.of(1, 2, 3); // [1, 2, 3]
 
-Array(7);          // array of 7 empty slots
-Array(1, 2, 3);    // [1, 2, 3]
+// Array(7);          // array of 7 empty slots
+// Array(1, 2, 3);    // [1, 2, 3]
+
+if (!Array.of) {
+    Array.of = function() {
+      return Array.prototype.slice.call(arguments);
+      // Or 
+      let vals = [];
+      for(let prop in arguments){
+          vals.push(arguments[prop]);
+      }
+      return vals;
+    }
+  }
