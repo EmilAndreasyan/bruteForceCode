184c3a7db2ea0cf528768b794a5d1b076b0ea987
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2020-11-09 20:32:51 -0800

    concat arrays

--- code.js
+++ code.js
@@ -749,10 +749,16 @@
 //     }
 //   }
 
-const array1 = ['a', 'b', 'c'];
-const array2 = ['d', 'e', 'f'];
-const array3 = array1.concat(array2);
+// const array1 = ['a', 'b', 'c'];
+// const array2 = ['d', 'e', 'f'];
+// const array3 = array1.concat(array2);
 
-console.log(array3);
-// expected output: Array ["a", "b", "c", "d", "e", "f"]
+// console.log(array3);
+// // expected output: Array ["a", "b", "c", "d", "e", "f"]
+
+const letters = ['a', 'b', 'c'];
+const numbers = [1, 2, 3];
+
+letters.concat(numbers);
+// result in ['a', 'b', 'c', 1, 2, 3]
 
