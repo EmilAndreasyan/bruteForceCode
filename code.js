abaa7f3bf1ad5d7a563ed03a63a7a3a3743d0555
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2020-11-10 19:08:02 -0800

    concat

--- code.js
+++ code.js
@@ -756,9 +756,23 @@
 // console.log(array3);
 // // expected output: Array ["a", "b", "c", "d", "e", "f"]
 
-const letters = ['a', 'b', 'c'];
-const numbers = [1, 2, 3];
+// const letters = ['a', 'b', 'c'];
+// const numbers = [1, 2, 3];
 
-letters.concat(numbers);
-// result in ['a', 'b', 'c', 1, 2, 3]
+// letters.concat(numbers);
+// // result in ['a', 'b', 'c', 1, 2, 3]
+
+const num1 = [[1]];
+const num2 = [2, [3]];
+
+const numbers = num1.concat(num2);
+
+console.log(numbers);
+// results in [[1], 2, [3]]
+
+// modify the first element of num1
+num1[0].push(4);
+
+console.log(numbers);
+// results in [[1, 4], 2, [3]]
 
