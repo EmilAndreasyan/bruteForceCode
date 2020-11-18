9d19e55edf5352b31dceb73606c01a73b6a098f1
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2020-11-17 16:50:08 -0800

    entries

--- code.js
+++ code.js
@@ -786,29 +786,50 @@
 // console.log(array1.copyWithin(1, 3));
 // // expected output: Array ["d", "d", "e", "d", "e"]
 
-[1, 2, 3, 4, 5].copyWithin(-2)
-// [1, 2, 3, 1, 2]
+// [1, 2, 3, 4, 5].copyWithin(-2)
+// // [1, 2, 3, 1, 2]
 
-[1, 2, 3, 4, 5].copyWithin(0, 3)
-// [4, 5, 3, 4, 5]
+// [1, 2, 3, 4, 5].copyWithin(0, 3)
+// // [4, 5, 3, 4, 5]
 
-[1, 2, 3, 4, 5].copyWithin(0, 3, 4)
-// [4, 2, 3, 4, 5]
+// [1, 2, 3, 4, 5].copyWithin(0, 3, 4)
+// // [4, 2, 3, 4, 5]
 
-[1, 2, 3, 4, 5].copyWithin(-2, -3, -1)
-// [1, 2, 3, 3, 4]
+// [1, 2, 3, 4, 5].copyWithin(-2, -3, -1)
+// // [1, 2, 3, 3, 4]
 
-[].copyWithin.call({length: 5, 3: 1}, 0, 3)
-// {0: 1, 3: 1, length: 5}
+// [].copyWithin.call({length: 5, 3: 1}, 0, 3)
+// // {0: 1, 3: 1, length: 5}
 
-// ES2015 Typed Arrays are subclasses of Array
-var i32a = new Int32Array([1, 2, 3, 4, 5])
+// // ES2015 Typed Arrays are subclasses of Array
+// var i32a = new Int32Array([1, 2, 3, 4, 5])
 
-i32a.copyWithin(0, 2)
-// Int32Array [3, 4, 5, 4, 5]
+// i32a.copyWithin(0, 2)
+// // Int32Array [3, 4, 5, 4, 5]
 
-// On platforms that are not yet ES2015 compliant: 
-[].copyWithin.call(new Int32Array([1, 2, 3, 4, 5]), 0, 3, 4);
-// Int32Array [4, 2, 3, 4, 5]
+// // On platforms that are not yet ES2015 compliant: 
+// [].copyWithin.call(new Int32Array([1, 2, 3, 4, 5]), 0, 3, 4);
+// // Int32Array [4, 2, 3, 4, 5]
 
+// nums = [2,7,11,15]
+// target = 9
+// var twoSum = function(nums, target) {
+//     let found = []
+//     nums.forEach((element, i) => {
+//         if (i) {
+            
+//         }
+//     });
+//   }
 
+// console.log(twoSum(nums, target))
+
+const array1 = ['a', 'b', 'c'];
+
+const iterator1 = array1.entries();
+
+console.log(iterator1.next().value);
+// expected output: Array [0, "a"]
+
+console.log(iterator1.next().value);
+// expected output: Array [1, "b"]
