213242dded77f79dd681a547a38dc6c043ad7b43
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2020-11-30 18:04:38 -0800

    function filterItems

--- code.js
+++ code.js
@@ -872,46 +872,60 @@ console.log(twoPointers(arr, target))
 //   [12, 5, 8, 130, 44].every(isBigEnough);   // false
 //   [12, 54, 18, 130, 44].every(isBigEnough); // true
 
-const array1 = [1, 2, 3, 4];
+// const array1 = [1, 2, 3, 4];
 
-// fill with 0 from position 2 until position 4
-console.log(array1.fill(0, 2, 4));
-// expected output: [1, 2, 0, 0]
+// // fill with 0 from position 2 until position 4
+// console.log(array1.fill(0, 2, 4));
+// // expected output: [1, 2, 0, 0]
 
-// fill with 5 from position 1
-console.log(array1.fill(5, 1));
-// expected output: [1, 5, 5, 5]
+// // fill with 5 from position 1
+// console.log(array1.fill(5, 1));
+// // expected output: [1, 5, 5, 5]
 
-console.log(array1.fill(6));
-// expected output: [6, 6, 6, 6]
+// console.log(array1.fill(6));
+// // expected output: [6, 6, 6, 6]
 
-const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
+// const words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present'];
 
-const result = words.filter(word => word.length > 6);
+// const result = words.filter(word => word.length > 6);
 
-console.log(result);
-// expected output: Array ["exuberant", "destruction", "present"]
+// console.log(result);
+// // expected output: Array ["exuberant", "destruction", "present"]
 
-const names = ['Emil', 'Tatevik', 'Adele', 'Victoria', 'Michael']
-const filteredNames = names.filter(name => name.length > 5);
-console.log(filteredNames)
+// const names = ['Emil', 'Tatevik', 'Adele', 'Victoria', 'Michael']
+// const filteredNames = names.filter(name => name.length > 5);
+// console.log(filteredNames)
 
-function isBigEnough(value) {
-    return value >= 10
-  }
+// function isBigEnough(value) {
+//     return value >= 10
+//   }
   
-  let filtered = [12, 5, 8, 130, 44].filter(isBigEnough)
-  // filtered is [12, 130, 44]
+//   let filtered = [12, 5, 8, 130, 44].filter(isBigEnough)
+//   // filtered is [12, 130, 44]
 
-const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]; 
+// const array = [-3, -2, -1, 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]; 
 
-function isPrime(num) {
-  for (let i = 2; num > i; i++) {
-    if (num % i == 0) {
-      return false;
-    }
-  }
-  return num > 1;
+// function isPrime(num) {
+//   for (let i = 2; num > i; i++) {
+//     if (num % i == 0) {
+//       return false;
+//     }
+//   }
+//   return num > 1;
+// }
+
+// console.log(array.filter(isPrime)); // [2, 3, 5, 7, 11, 13]
+
+let fruits = ['apple', 'banana', 'grapes', 'mango', 'orange']
+
+/**
+ * Filter array items based on search criteria (query)
+ */
+function filterItems(arr, query) {
+  return arr.filter(function(el) {
+      return el.toLowerCase().indexOf(query.toLowerCase()) !== -1
+  })
 }
 
-console.log(array.filter(isPrime)); // [2, 3, 5, 7, 11, 13]
+console.log(filterItems(fruits, 'ap'))  // ['apple', 'grapes']
+console.log(filterItems(fruits, 'an'))  // ['banana', 'mango', 'orange']
