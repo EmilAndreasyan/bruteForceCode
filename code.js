6ec4111f5e18d0fcf605ddfc3fbf79575835e61d
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2020-12-02 18:41:04 -0800

    appended words

--- code.js
+++ code.js
@@ -947,3 +947,14 @@ const modifiedWords = words.filter( (word, index, arr) => {
 console.log(modifiedWords)
 // Notice there are three words below length 6, but since they've been modified one is returned
 // ["spray"]
+
+// Appending new words
+words = ['spray', 'limit', 'exuberant', 'destruction','elite', 'present']
+const appendedWords = words.filter( (word, index, arr) => {
+  arr.push('new')
+  return word.length < 6
+})
+
+console.log(appendedWords)
+// Only three fits the condition even though the `words` itself now has a lot more words with character length less than 6
+// ["spray" ,"limit" ,"elite"]
