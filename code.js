53cd7fe21d8ad82b4e31f435e65f98d27083ef81
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2020-12-11 17:50:07 -0800

    prime number

--- code.js
+++ code.js
@@ -1025,12 +1025,25 @@ console.log(moveZeros(nums))
 
 // console.log(inventory.find(isCherries))
 
-const inventory = [
-  {name: 'apples', quantity: 2},
-  {name: 'bananas', quantity: 0},
-  {name: 'cherries', quantity: 5}
-];
+// const inventory = [
+//   {name: 'apples', quantity: 2},
+//   {name: 'bananas', quantity: 0},
+//   {name: 'cherries', quantity: 5}
+// ];
 
-const result = inventory.find( ({ name }) => name === 'cherries' );
+// const result = inventory.find( ({ name }) => name === 'cherries' );
 
-console.log(result) // { name: 'cherries', quantity: 5 }
+// console.log(result) // { name: 'cherries', quantity: 5 }
+
+function isPrime(element, index, array) {
+  let start = 2;
+  while (start <= Math.sqrt(element)) {
+    if (element % start++ < 1) {
+      return false;
+    }
+  }
+  return element > 1;
+}
+
+console.log([4, 6, 8, 12].find(isPrime)); // undefined, not found
+console.log([4, 5, 8, 12].find(isPrime)); // 5
