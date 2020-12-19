54267cedd99bc30682cdf02686d14c33dd2240fd
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2020-12-18 21:19:20 -0800

    flat reduce

--- code.js
+++ code.js
@@ -1083,3 +1083,14 @@ const arr2 = [0, 1, 2, [[[3, 4]]]];
 
 console.log(arr2.flat(2));
 // expected output: [0, 1, 2, [3, 4]]
+
+const arr = [1, 2, [3, 4]];
+
+// To flat single level array
+arr.flat();
+// is equivalent to
+arr.reduce((acc, val) => acc.concat(val), []);
+// [1, 2, 3, 4]
+
+// or with decomposition syntax
+const flattened = arr => [].concat(...arr);
