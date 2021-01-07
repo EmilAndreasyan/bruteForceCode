f053d8484f26eea6073588f526a20a5a021695a1
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2021-01-06 19:46:13 -0800

    flatten

--- code.js
+++ code.js
@@ -1261,12 +1261,31 @@ console.log(moveZeros(nums))
 // const obj1 = { a: 1, b: 2 }
 // const obj2 = copy(obj1) // obj2 looks like obj1 now
 
-let words = ['one', 'two', 'three', 'four']
-words.forEach(function(word) {
-  console.log(word)
-  if (word === 'two') {
-    words.shift() //'one' will delete from array
-  }
-}) // one // two // four
+// let words = ['one', 'two', 'three', 'four']
+// words.forEach(function(word) {
+//   console.log(word)
+//   if (word === 'two') {
+//     words.shift() //'one' will delete from array
+//   }
+// }) // one // two // four
 
-console.log(words);  //['two', 'three', 'four']
+// console.log(words);  //['two', 'three', 'four']
+
+function flatten(arr) {
+  const result = []
+
+  arr.forEach(function(i) {
+    if (Array.isArray(i)) {
+      result.push(...flatten(i))
+    } else {
+      result.push(i)
+    }
+  })
+
+  return result
+}
+
+// Usage
+const nested = [1, 2, 3, [4, 5, [6, 7], 8, 9]]
+
+flatten(nested) // [1, 2, 3, 4, 5, 6, 7, 8, 9]
