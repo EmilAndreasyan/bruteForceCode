d5b24d52e50256c16029c0bfc5721904219ad275
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2021-01-21 17:35:13 -0800

    includes4

--- code.js
+++ code.js
@@ -1381,3 +1381,14 @@ let arr = ['a', 'b', 'c']
 
 arr.includes('c', 3)    // false
 arr.includes('c', 100)  // false
+
+// array length is 3
+// fromIndex is -100
+// computed index is 3 + (-100) = -97
+
+let arr = ['a', 'b', 'c']
+
+arr.includes('a', -100) // true
+arr.includes('b', -100) // true
+arr.includes('c', -100) // true
+arr.includes('a', -2)   // false
