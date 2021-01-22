751ef6b47d6cc415e7a9d9589be14c2bb7b895a0
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2021-01-21 17:33:42 -0800

    includes

--- code.js
+++ code.js
@@ -1327,23 +1327,36 @@ Array.from([1, 2, 3], x => x + x);
 // Generate a sequence of numbers
 // Since the array is initialized with `undefined` on each position,
 // the value of `v` below will be `undefined`
-Array.from({length: 5}, (v, i) => i);
-// [0, 1, 2, 3, 4]
+// Array.from({length: 5}, (v, i) => i);
+// // [0, 1, 2, 3, 4]
 
-// Sequence generator function (commonly referred to as "range", e.g. Clojure, PHP etc)
-const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
+// // Sequence generator function (commonly referred to as "range", e.g. Clojure, PHP etc)
+// const range = (start, stop, step) => Array.from({ length: (stop - start) / step + 1}, (_, i) => start + (i * step));
 
-// Generate numbers range 0..4
-range(0, 4, 1);
-// [0, 1, 2, 3, 4]
+// // Generate numbers range 0..4
+// range(0, 4, 1);
+// // [0, 1, 2, 3, 4]
 
-// Generate numbers range 1..10 with step of 2
-range(1, 10, 2);
-// [1, 3, 5, 7, 9]
+// // Generate numbers range 1..10 with step of 2
+// range(1, 10, 2);
+// // [1, 3, 5, 7, 9]
 
-// Generate the alphabet using Array.from making use of it being ordered as a sequence
-range('A'.charCodeAt(0), 'Z'.charCodeAt(0), 1).map(x => String.fromCharCode(x));
-// ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+// // Generate the alphabet using Array.from making use of it being ordered as a sequence
+// range('A'.charCodeAt(0), 'Z'.charCodeAt(0), 1).map(x => String.fromCharCode(x));
+// // ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+
+// const array1 = [1, 2, 3];
+
+// console.log(array1.includes(2));
+// // expected output: true
+
+// const pets = ['cat', 'dog', 'bat'];
+
+// console.log(pets.includes('cat'));
+// // expected output: true
+
+// console.log(pets.includes('at'));
+// // expected output: false
 
 const array1 = [1, 2, 3];
 
