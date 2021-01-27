ba5fe866c36954a264e064e7a2994f35437074a6
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2021-01-26 18:16:04 -0800

    indexOf2

--- code.js
+++ code.js
@@ -1414,3 +1414,10 @@ console.log(beasts.indexOf('bison', 2));
 
 console.log(beasts.indexOf('giraffe'));
 // expected output: -1
+
+var array = [2, 9, 9];
+array.indexOf(2);     // 0
+array.indexOf(7);     // -1
+array.indexOf(9, 2);  // 2
+array.indexOf(2, -1); // -1
+array.indexOf(2, -3); // 0
