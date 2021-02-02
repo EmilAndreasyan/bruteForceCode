74b227af948d9d3af1232a02123b7fe1c1f7f732
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2021-02-01 19:17:04 -0800

    updateVegetablesCollection

--- code.js
+++ code.js
@@ -1398,42 +1398,17 @@ Array.from([1, 2, 3], x => x + x);
 //   console.log(Array.prototype.includes.call(arguments, 'd'))  // false
 // })('a','b','c') 
 
-(function() {
-  console.log(Array.prototype.includes.call(arguments, 'a'))  // true
-  console.log(Array.prototype.includes.call(arguments, 'd'))  // false
-})('a','b','c') 
+// (function() {
+//   console.log(Array.prototype.includes.call(arguments, 'a'))  // true
+//   console.log(Array.prototype.includes.call(arguments, 'd'))  // false
+// })('a','b','c') 
 
-const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
+// const beasts = ['ant', 'bison', 'camel', 'duck', 'bison'];
 
-console.log(beasts.indexOf('bison'));
-// expected output: 1
+// console.log(beasts.indexOf('bison'));
+// // expected output: 1
 
-// start from index 2
-console.log(beasts.indexOf('bison', 2));
-// expected output: 4
-
-console.log(beasts.indexOf('giraffe'));
-// expected output: -1
-
-var array = [2, 9, 9];
-array.indexOf(2);     // 0
-array.indexOf(7);     // -1
-array.indexOf(9, 2);  // 2
-array.indexOf(2, -1); // -1
-array.indexOf(2, -3); // 0
-
-var indices = [];
-var array = ['a', 'b', 'a', 'c', 'a', 'd'];
-var element = 'a';
-var idx = array.indexOf(element);
-while (idx != -1) {
-  indices.push(idx);
-  idx = array.indexOf(element, idx + 1);
-}
-console.log(indices);
-// [0, 2, 4]
-
-unction updateVegetablesCollection (veggies, veggie) {
+function updateVegetablesCollection (veggies, veggie) {
   if (veggies.indexOf(veggie) === -1) {
       veggies.push(veggie);
       console.log('New veggies collection is : ' + veggies);
@@ -1448,3 +1423,44 @@ updateVegetablesCollection(veggies, 'spinach');
 // New veggies collection is : potato,tomato,chillies,green-pepper,spinach
 updateVegetablesCollection(veggies, 'spinach');
 // spinach already exists in the veggies collection.
+
+// // start from index 2
+// console.log(beasts.indexOf('bison', 2));
+// // expected output: 4
+
+// console.log(beasts.indexOf('giraffe'));
+// // expected output: -1
+
+// var array = [2, 9, 9];
+// array.indexOf(2);     // 0
+// array.indexOf(7);     // -1
+// array.indexOf(9, 2);  // 2
+// array.indexOf(2, -1); // -1
+// array.indexOf(2, -3); // 0
+
+// var indices = [];
+// var array = ['a', 'b', 'a', 'c', 'a', 'd'];
+// var element = 'a';
+// var idx = array.indexOf(element);
+// while (idx != -1) {
+//   indices.push(idx);
+//   idx = array.indexOf(element, idx + 1);
+// }
+// console.log(indices);
+// // [0, 2, 4]
+
+// function updateVegetablesCollection (veggies, veggie) {
+//   if (veggies.indexOf(veggie) === -1) {
+//       veggies.push(veggie);
+//       console.log('New veggies collection is : ' + veggies);
+//   } else if (veggies.indexOf(veggie) > -1) {
+//       console.log(veggie + ' already exists in the veggies collection.');
+//   }
+// }
+
+// var veggies = ['potato', 'tomato', 'chillies', 'green-pepper'];
+
+// updateVegetablesCollection(veggies, 'spinach');
+// // New veggies collection is : potato,tomato,chillies,green-pepper,spinach
+// updateVegetablesCollection(veggies, 'spinach');
+// // spinach already exists in the veggies collection.
