a9cd7f6e92da20ba76ae467070aa289415dc34ac
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2021-02-10 21:45:26 -0800

    keys

--- code.js
+++ code.js
@@ -1541,3 +1541,14 @@ function f(a, b, c) {
 }
 f(1, 'a', true);
 //expected output: "1,a,true"
+
+const array1 = ['a', 'b', 'c'];
+const iterator = array1.keys();
+
+for (const key of iterator) {
+  console.log(key);
+}
+
+// expected output: 0
+// expected output: 1
+// expected output: 2
