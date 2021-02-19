392d4a6f9cf500fb1eccfc08daf3542c653f8f75
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2021-02-18 19:02:29 -0800

    Object.keys

--- code.js
+++ code.js
@@ -1553,13 +1553,19 @@ Array.from([1, 2, 3], x => x + x);
 // // expected output: 1
 // // expected output: 2
 
-const array1 = ['a', 'b', 'c'];
-const iterator = array1.keys();
+// const array1 = ['a', 'b', 'c'];
+// const iterator = array1.keys();
 
-for (const key of iterator) {
-  console.log(key);
-}
+// for (const key of iterator) {
+//   console.log(key);
+// }
 
-// expected output: 0
-// expected output: 1
-// expected output: 2
+// // expected output: 0
+// // expected output: 1
+// // expected output: 2
+
+var arr = ['a', , 'c'];
+var sparseKeys = Object.keys(arr);
+var denseKeys = [...arr.keys()];
+console.log(sparseKeys); // ['0', '2']
+console.log(denseKeys);  // [0, 1, 2]
