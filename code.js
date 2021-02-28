499da37759bff80df3bda833cb17a4835d267871
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2021-02-28 13:20:12 -0800

    indices

--- code.js
+++ code.js
@@ -1564,24 +1564,36 @@ Array.from([1, 2, 3], x => x + x);
 // // expected output: 1
 // // expected output: 2
 
-var arr = ['a', , 'c'];
-var sparseKeys = Object.keys(arr);
-var denseKeys = [...arr.keys()];
-console.log(sparseKeys); // ['0', '2']
-console.log(denseKeys);  // [0, 1, 2]
+// var arr = ['a', , 'c'];
+// var sparseKeys = Object.keys(arr);
+// var denseKeys = [...arr.keys()];
+// console.log(sparseKeys); // ['0', '2']
+// console.log(denseKeys);  // [0, 1, 2]
 
-const animals = ['Dodo', 'Tiger', 'Penguin', 'Dodo'];
+// const animals = ['Dodo', 'Tiger', 'Penguin', 'Dodo'];
 
-console.log(animals.lastIndexOf('Dodo'));
-// expected output: 3
+// console.log(animals.lastIndexOf('Dodo'));
+// // expected output: 3
 
-console.log(animals.lastIndexOf('Tiger'));
-// expected output: 1
+// console.log(animals.lastIndexOf('Tiger'));
+// // expected output: 1
 
-var numbers = [2, 5, 9, 2];
-numbers.lastIndexOf(2);     // 3
-numbers.lastIndexOf(7);     // -1
-numbers.lastIndexOf(2, 3);  // 3
-numbers.lastIndexOf(2, 2);  // 0
-numbers.lastIndexOf(2, -2); // 0
-numbers.lastIndexOf(2, -1); // 3
+// var numbers = [2, 5, 9, 2];
+// numbers.lastIndexOf(2);     // 3
+// numbers.lastIndexOf(7);     // -1
+// numbers.lastIndexOf(2, 3);  // 3
+// numbers.lastIndexOf(2, 2);  // 0
+// numbers.lastIndexOf(2, -2); // 0
+// numbers.lastIndexOf(2, -1); // 3
+
+var indices = [];
+var array = ['a', 'b', 'a', 'c', 'a', 'd'];
+var element = 'a';
+var idx = array.lastIndexOf(element);
+while (idx != -1) {
+  indices.push(idx);
+  idx = (idx > 0 ? array.lastIndexOf(element, idx - 1) : -1);
+}
+
+console.log(indices);
+// [4, 2, 0]
