2f93cdebb31ae601ed16522a52cd58a6832872c3
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2021-02-28 13:21:22 -0800

    map

--- code.js
+++ code.js
@@ -1597,3 +1597,11 @@ while (idx != -1) {
 
 console.log(indices);
 // [4, 2, 0]
+
+const array1 = [1, 4, 9, 16];
+
+// pass a function to map
+const map1 = array1.map(x => x * 2);
+
+console.log(map1);
+// expected output: Array [2, 8, 18, 32]
