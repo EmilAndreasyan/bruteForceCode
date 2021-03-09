daf92023e81ade3f84118bed01b0c13e6da83e95
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2021-03-08 21:21:54 -0800

    kvArray

--- code.js
+++ code.js
@@ -1612,3 +1612,19 @@ let roots = numbers.map(function(num) {
 })
 // roots is now     [1, 2, 3]
 // numbers is still [1, 4, 9]
+
+let kvArray = [{key: 1, value: 10},
+  {key: 2, value: 20},
+  {key: 3, value: 30}]
+
+let reformattedArray = kvArray.map(obj => {
+let rObj = {}
+rObj[obj.key] = obj.value
+return rObj
+})
+// reformattedArray is now [{1: 10}, {2: 20}, {3: 30}],
+
+// kvArray is still:
+// [{key: 1, value: 10},
+//  {key: 2, value: 20},
+//  {key: 3, value: 30}]
