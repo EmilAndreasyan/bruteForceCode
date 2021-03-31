2a1bef93e7950523c7f418c2e8727e9bf2f3c3d6
Emil_Andreasyan <emil.andreasyan@yahoo.com>
2021-03-30 19:13:51 -0700

    pop3

--- code.js
+++ code.js
@@ -1684,3 +1684,11 @@ var popped = myFish.pop();
 console.log(myFish); // ['angel', 'clown', 'mandarin' ]
 
 console.log(popped); // 'sturgeon'
+
+var myFish = {0:'angel', 1:'clown', 2:'mandarin', 3:'sturgeon', length: 4};
+
+var popped = Array.prototype.pop.call(myFish); //same syntax for using apply( )
+
+console.log(myFish); // {0:'angel', 1:'clown', 2:'mandarin', length: 3}
+
+console.log(popped); // 'sturgeon'
